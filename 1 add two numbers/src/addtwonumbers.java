94caccd51cb66d256147d3c692f78cb340af8fa3
Tucker Campbell <tcampbell0092@gmail.com>
2019-10-13 11:48:19 -0500

    Started repository and added 1 add two numbers.java

---
+++ 1 add two numbers/src/addtwonumbers.java
@@ -0,0 +1,13 @@
+public class addtwonumbers {
+
+  //static void declaration
+  public static void main(String[] args) {
+
+      //function for adding two numbers
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      //prints out function
+      System.out.println("Sum of these numbers: " + sum);
+  }
+}
