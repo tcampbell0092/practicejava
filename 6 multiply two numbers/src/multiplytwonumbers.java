a3b875dc58146e76f5a2f167e609b5e3d6271423
Tucker Campbell <tcampbell0092@gmail.com>
2019-10-24 16:58:23 -0500

    Added 6 multiply two numbers.java

---
+++ 6 multiply two numbers/src/multiplytwonumbers.java
@@ -0,0 +1,13 @@
+public class multiplytwonumbers {
+
+    //static void declaration
+    public static void main(String[]args) {
+
+        //function for multiplying two numbers
+        int num1 = 10, num2 = 20, product;
+        product = num1 * num2;
+
+        //prints out function
+        System.out.println("Product of these numbers: " + product);
+    }
+}
