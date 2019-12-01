49ca1bd9306dc7c2aba3fce2387c383e65a979b9
Tucker Campbell <tcampbell0092@gmail.com>
2019-12-01 13:20:09 -0600

    Added 12 quotient and remainder.java

---
+++ 12 quotient and remainder/src/quoandremain.java
@@ -0,0 +1,14 @@
+public class quoandremain {
+    public static void main(String[]args){
+        // defines numbers to be divided
+        int num1 = 15, num2 = 2;
+        // output for quotient
+        int quotient = num1 / num2;
+        // output for remainder
+        int remainder = num1 % num2;
+
+        // prints out operations
+        System.out.println("Quotient of these numbers is: " + quotient);
+        System.out.println("Remainder of these numbers is: " + remainder);
+    }
+}
