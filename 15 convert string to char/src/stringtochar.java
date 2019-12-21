26156babdb4de70950b1f8fbf7a3edf09eed3ebf
Tucker Campbell <tcampbell0092@gmail.com>
2019-12-21 08:32:55 -0600

    Added 15 convert string to char.java

---
+++ 15 convert string to char/src/stringtochar.java
@@ -0,0 +1,12 @@
+public class stringtochar {
+    public static void main(String []args)
+    {
+        //using charAt() method
+        String str = "Hello";
+        for(int i=0; i<str.length(); i++)
+        {
+            char ch = str.charAt(i);
+            System.out.println("Character at " + i + " Position: " + ch);
+        }
+    }
+}
